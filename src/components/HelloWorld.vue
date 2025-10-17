47f18b04eef115902cb561726536ebb8918d30c4
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2025-10-17 18:27:44 -0300

    plugins (#2)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-form
+    :label-position="labelPosition"
+    label-width="auto"
+    :model="formLabelAlign"
+    style="max-width: 600px"
+  >
+    <el-form-item label="Form Align" label-position="right">
+      <el-radio-group v-model="labelPosition" aria-label="label position">
+        <el-radio-button value="left">Left</el-radio-button>
+        <el-radio-button value="right">Right</el-radio-button>
+        <el-radio-button value="top">Top</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Form Item Align" label-position="right">
+      <el-radio-group
+        v-model="itemLabelPosition"
+        aria-label="item label position"
+      >
+        <el-radio-button value="">Empty</el-radio-button>
+        <el-radio-button value="left">Left</el-radio-button>
+        <el-radio-button value="right">Right</el-radio-button>
+        <el-radio-button value="top">Top</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="Name" :label-position="itemLabelPosition">
+      <el-input v-model="formLabelAlign.name" />
+    </el-form-item>
+    <el-form-item label="Activity zone" :label-position="itemLabelPosition">
+      <el-input v-model="formLabelAlign.region" />
+    </el-form-item>
+    <el-form-item label="Activity form" :label-position="itemLabelPosition">
+      <el-input v-model="formLabelAlign.type" />
+    </el-form-item>
+  </el-form>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+<script setup>
+import { reactive, ref } from 'vue'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+import { FormItemProps, FormProps } from 'element-plus'
+
+const labelPosition = ref<FormProps['labelPosition']>('right')
+const itemLabelPosition = ref<FormItemProps['labelPosition']>('')
+const formLabelAlign = reactive({
+  name: '',
+  region: '',
+  type: '',
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import naive from 'naive-ui'
+
+import vuetify from './plugins/vuetify'
+
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .use(ElementPlus)
+  .use(naive)
+  .mount('#app')
