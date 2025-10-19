e1b1329bf4f3ffe0cf8ebf015657bc1be537846b
Jeffersson Barros <146139830+Jeffersson17@users.noreply.github.com>
2025-10-19 19:29:34 -0300

    cleaner_project (#1)

--- src/layouts/full/Main.vue
+++ src/layouts/full/Main.vue
@@ -1,31 +1,26 @@
-<script setup lang="ts">
+<script setup>
 import { ref, shallowRef } from 'vue';
 import sidebarItems from './vertical-sidebar/sidebarItem';
 import NavGroup from './vertical-sidebar/NavGroup/index.vue';
 import NavItem from './vertical-sidebar/NavItem/index.vue';
 import Logo from './logo/Logo.vue';
 // Icon Imports
-import { Menu2Icon, BellRingingIcon } from 'vue-tabler-icons';
 import NotificationDD from './vertical-header/NotificationDD.vue';
 import ProfileDD from './vertical-header/ProfileDD.vue';
 const sidebarMenu = shallowRef(sidebarItems);
 const sDrawer = ref(true);
-
 </script>
 
 <template>
-    <v-navigation-drawer left v-model="sDrawer" app class="leftSidebar ml-sm-5 mt-sm-5 bg-containerBg" elevation="10"
-        width="270">
-        <div class="pa-5 pl-4 ">
+    <v-navigation-drawer left v-model="sDrawer" app class="leftSidebar ml-sm-5 mt-sm-5 bg-containerBg" elevation="10" width="270">
+        <div class="pa-5 pl-4">
             <Logo />
         </div>
-        <!-- ---------------------------------------------- -->
         <!---Navigation -->
-        <!-- ---------------------------------------------- -->
         <perfect-scrollbar class="scrollnavbar bg-containerBg overflow-y-hidden">
             <v-list class="py-4 px-4 bg-containerBg">
                 <!---Menu Loop -->
-                <template v-for="(item, i) in sidebarMenu">
+                <template v-for="item in sidebarMenu">
                     <!---Item Sub Header -->
                     <NavGroup :item="item" v-if="item.header" :key="item.title" />
                     <!---Single Item-->
@@ -34,11 +29,6 @@ const sDrawer = ref(true);
                 </template>
                 <!-- <Moreoption/> -->
             </v-list>
-            <div class="py-0 px-6">
-                <v-btn class="mr-2 bg-primary rounded-pill" size="large"
-                    href="https://www.wrappixel.com/templates/spike-vuejs-admin-dashboard/?ref=33" block target="_blank">Upgrade to
-                    Pro</v-btn>
-            </div>
         </perfect-scrollbar>
     </v-navigation-drawer>
     <div class="container verticalLayout">
@@ -46,18 +36,13 @@ const sDrawer = ref(true);
             <v-app-bar elevation="0" height="70">
                 <div class="d-flex align-center justify-space-between w-100">
                     <div>
-                        <v-btn class="hidden-lg-and-up text-muted" @click="sDrawer = !sDrawer" icon
-                            variant="flat" size="small">
+                        <v-btn class="hidden-lg-and-up text-muted" @click="sDrawer = !sDrawer" icon variant="flat" size="small">
                             <Menu2Icon size="20" stroke-width="1.5" />
                         </v-btn>
                         <!-- Notification -->
                         <NotificationDD />
                     </div>
                     <div>
-                        <!-- Upgrade button -->
-                        <v-btn class="mr-2 bg-primary rounded-pill"
-                            href="https://www.wrappixel.com/templates/spike-vuejs-admin-dashboard/?ref=33"
-                            target="_blank">Upgrade to Pro</v-btn>
                         <!-- User Profile -->
                         <ProfileDD />
                     </div>
--- src/layouts/full/vertical-header/ProfileDD.vue
+++ src/layouts/full/vertical-header/ProfileDD.vue
@@ -1,16 +1,13 @@
-<script setup lang="ts">
-import { UserIcon, MailIcon, ListCheckIcon } from 'vue-tabler-icons';
+<script setup>
 </script>
 
 <template>
-    <!-- ---------------------------------------------- -->
     <!-- notifications DD -->
-    <!-- ---------------------------------------------- -->
     <v-menu :close-on-content-click="false">
         <template v-slot:activator="{ props }">
             <v-btn class="" variant="text" v-bind="props" icon>
                 <v-avatar size="35">
-                    <img src="@/assets/images/profile/user-1.jpg" height="35" alt="user" />
+                    <img src="../../../assets/images/profile/user_default.jpg" height="35" alt="user" />
                 </v-avatar>
             </v-btn>
         </template>
@@ -28,7 +25,7 @@ import { UserIcon, MailIcon, ListCheckIcon } from 'vue-tabler-icons';
                     </template>
                     <v-list-item-title  class="pl-4 text-body-1">My Account</v-list-item-title>
                 </v-list-item>
-                <v-list-item value="item3" color="primary"> 
+                <v-list-item value="item3" color="primary">
                     <template v-slot:prepend>
                         <ListCheckIcon stroke-width="1.5"  size="20"/>
                     </template>
