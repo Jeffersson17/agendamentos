022f1eda4558c96320924c0622183df6b74ab570
Jeffersson <barrosjefferson407@gmail.com>
2025-10-21 17:11:00 -0300

    change sidebarItens

--- src/layouts/full/vertical-sidebar/sidebarItem.ts
+++ src/layouts/full/vertical-sidebar/sidebarItem.ts
@@ -1,90 +1,69 @@
-import {
-  LayoutDashboardIcon,BorderAllIcon,
-  AlertCircleIcon,
-  CircleDotIcon,
-  BoxMultiple1Icon,
-  LoginIcon, MoodHappyIcon, ApertureIcon, UserPlusIcon
-} from 'vue-tabler-icons';
+import { LayoutDashboardIcon, CalendarEventIcon, UsersIcon, ClockIcon, CreditCardIcon, BuildingIcon, ScissorsIcon } from 'vue-tabler-icons';
 
 export interface menu {
-  header?: string;
-  title?: string;
-  icon?: any;
-  to?: string;
-  chip?: string;
-  BgColor?: string;
-  chipBgColor?: string;
-  chipColor?: string;
-  chipVariant?: string;
-  chipIcon?: string;
-  children?: menu[];
-  disabled?: boolean;
-  type?: string;
-  subCaption?: string;
+    header?: string;
+    title?: string;
+    icon?: any;
+    to?: string;
+    chip?: string;
+    BgColor?: string;
+    chipBgColor?: string;
+    chipColor?: string;
+    chipVariant?: string;
+    chipIcon?: string;
+    children?: menu[];
+    disabled?: boolean;
+    type?: string;
+    subCaption?: string;
 }
 
 const sidebarItem: menu[] = [
-  { header: 'Home' },
-  {
-    title: 'Dashboard',
-    icon: LayoutDashboardIcon,
-    BgColor: 'primary',
-    to: '/'
-  },
-  { header: 'Ui components' },
-  {
-    title: "Alert",
-    icon: AlertCircleIcon,
-    BgColor: 'primary',
-    to: "/ui/alerts",
-    
-  },
-  {
-    title: "Button",
-    icon: CircleDotIcon,
-    BgColor: 'primary',
-    to: "/ui/buttons",
-  },
-  {
-    title: "Cards",
-    icon: BoxMultiple1Icon,
-    BgColor: 'primary',
-    to: "/ui/cards",
-  },
-  {
-    title: "Tables",
-    icon: BorderAllIcon,
-    BgColor: 'primary',
-    to: "/ui/tables",
-  },
+    { header: 'Home' },
+    {
+        title: 'Painel de Controle',
+        icon: LayoutDashboardIcon,
+        BgColor: 'primary',
+        to: '/'
+    },
+    { header: 'Barbearia' },
+    {
+        title: 'Agendamentos',
+        icon: CalendarEventIcon,
+        BgColor: 'primary',
+        to: '/ui/alerts'
+    },
+    {
+        title: 'Serviços',
+        icon: ScissorsIcon,
+        BgColor: 'primary',
+        to: '/ui/buttons'
+    },
+    {
+        title: 'Clientes',
+        icon: UsersIcon,
+        BgColor: 'primary',
+        to: '/ui/cards'
+    },
+    {
+        title: 'Horarios',
+        icon: ClockIcon,
+        BgColor: 'primary',
+        to: '/ui/tables'
+    },
 
-  { header: 'Auth' },
-  {
-    title: 'Login',
-    icon: LoginIcon,
-    BgColor: 'primary',
-    to: '/auth/login'
-},
-{
-    title: 'Register',
-    icon: UserPlusIcon,
-    BgColor: 'primary',
-    to: '/auth/register'
-},
-{ header: 'Extra' },
-{
-    title: 'Icons',
-    icon: MoodHappyIcon,
-    BgColor: 'primary',
-    to: '/icons'
-},
-{
-    title: 'Sample Page',
-    icon: ApertureIcon,
-    BgColor: 'primary',
-    to: '/sample-page'
-},
-
+    // { header: 'Administração' },
+    // {
+    //     title: 'Pagamentos',
+    //     icon: CreditCardIcon,
+    //     BgColor: 'primary',
+    //     to: '/auth/login'
+    // },
+    // {
+    //     title: 'Empresas',
+    //     icon: BuildingIcon,
+    //     BgColor: 'primary',
+    //     to: '/auth/register'
+    // }
 ];
 
 export default sidebarItem;
